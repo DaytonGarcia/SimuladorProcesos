4b3e6de0901d909570efec53e9ed18031a817672
Dayton <dayton.garcia@gmail.com>
2016-05-12 22:28:54 -0600

    Modifico Compilador Simulacion

---
+++ src/AnalizadorSimulacion/ProcesoS.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AnalizadorSimulacion;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dayton
+ */
+public class ProcesoS {
+    
+    int id;
+    ArrayList<Recurso> recursos = new ArrayList<Recurso>();
+    
+    ProcesoS(int id){
+        this.id = id;
+    }
+    
+    
+}
